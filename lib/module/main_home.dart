267313763e1a4f846de5c5523a15cf844de412a1
QuikAppTech <prasannasrie@gmail.com>
2025-05-08 22:13:57 +0530

    Auto Sign Code 49

--- lib/module/main_home.dart
+++ lib/module/main_home.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
@@ -13,9 +14,10 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:permission_handler/permission_handler.dart';
 
-import '../config/env_config.dart';
-import '../services/notification_service.dart';
 
+import '../config/env_config.dart';
+
+import '../services/notification_service.dart';
 
 class MainHome extends StatefulWidget {
   final String webUrl;
@@ -63,6 +65,11 @@ class _MainHomeState extends State<MainHome> {
   final urlController = TextEditingController();
   DateTime? _lastBackPressed;
   String? _pendingInitialUrl; // 🔹 NEW
+  StreamSubscription? _linkSub;
+  String myDomain = "";
+  bool _initialUriIsHandled = false;
+
+  // String? _pendingInitialUrl;
 
   InAppWebViewSettings settings = InAppWebViewSettings(
     isInspectable: kDebugMode,
@@ -101,11 +108,11 @@ class _MainHomeState extends State<MainHome> {
     if (pushNotify == true) {
       setupFirebaseMessaging();
     }
+
     isBottomMenu = widget.isBottomMenu;
     if (isBottomMenu == true) {
       try {
         bottomMenuItems = widget.bottomMenuItems;
-        // bottomMenuItems = List<Map<String, dynamic>>.from(json.decode(bottomMenuRaw));
       } catch (e) {
         if (kDebugMode) {
           print("Invalid bottom menu JSON: $e");
@@ -157,7 +164,15 @@ class _MainHomeState extends State<MainHome> {
         await _showLocalNotification(message);
       }
     });
+    Uri parsedUri = Uri.parse(widget.webUrl);
+    myDomain = parsedUri.host;
+    if (myDomain.startsWith('www.')) {
+      myDomain = myDomain.substring(4);
+    }
   }
+
+
+
 
   IconData _getIconByName(String? name) {
     if (name == null || name.trim().isEmpty) {
@@ -400,7 +415,9 @@ class _MainHomeState extends State<MainHome> {
                         },
                         shouldOverrideUrlLoading: (controller, navigationAction) async {
                           final uri = navigationAction.request.url;
-                          if (uri != null && !uri.toString().contains(widget.webUrl)) {
+                          // if (uri != null && !uri.toString().contains(widget.webUrl)) {
+                          if (uri != null && !uri.host.contains(myDomain)) {
+
                             if (widget.isDeeplink) {
                               if (await canLaunchUrl(uri)) {
                                 await launchUrl(uri, mode: LaunchMode.externalApplication);
